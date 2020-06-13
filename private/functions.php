439b83a9ca67358280825957445988c990d394cb
Bill Liu Macbook <liuyuchen1997@hotmail.com>
2020-06-12 23:46:09 -0700

    Altered Cases Table and creation script, Added functions for query, finished viewcases page

--- private/functions.php
+++ private/functions.php
@@ -2,11 +2,47 @@
     require_once('connect.php');
 
     function logout() {
-        echo "hello";
         // destroy session
         session_destroy();
         // Redirect to login page
         header("location: index.php");
         exit;
+    }
+
+    function get_docid($userid) {
+        global $conn;
+        $sql = "select Did from Link_Doctor_Account ";
+        $sql .= "where UserName = '" .$userid. "';";
+        $result = mysqli_query($conn, $sql);
+        if (!$result) {
+            exit("Database query failed.");
+        }
+        $Did = mysqli_fetch_assoc($result)['Did'];
+        return $Did;
+    }
+
+    function get_existing_cases_table_set($Did) {
+        global $conn;
+        $sql = "select CaseNum, EncounterDate, FirstName, LastName ";
+        $sql .= "from Cases, Resident ";
+        $sql .= "where Did = '" . $Did . "' AND Cases.RID = Resident.Rid;";
+        $result = mysqli_query($conn, $sql);
+        if (!$result) {
+            exit("Database query failed.");
+        }
+        return $result;
+    }
+
+    function get_case_detail($casenum) {
+        global $conn;
+        $sql = "select CaseNum, EncounterDate, RecoveryStatus, Notes, FirstName, LastName ";
+        $sql .= "from Cases, Resident ";
+        $sql .= "where CaseNum = '" . $casenum . "' AND Cases.RID = Resident.Rid;";
+        $result = mysqli_query($conn, $sql);
+        if (!$result) {
+            exit("Database query failed.");
+        }
+        $case_detail = mysqli_fetch_assoc($result);
+        return $case_detail;
     }
 ?>
